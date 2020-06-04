827c56f4eea6dab4a53e2d5c72e5bff83ec44410
brgerig <brgerig@taylor.edu>
2020-06-04 12:56:37 -0400

    Update config.h
    Changes for Hue

--- config.h
+++ config.h
@@ -1,2 +1,6 @@
 #define SSID                          "wifissid"
 #define PASSWORD                      "wifipassword"
+
+// Hue settings
+#define HUE_IP                         "1.2.3.4"
+#define HUE_USER                       "HueUser"
