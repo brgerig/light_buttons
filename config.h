6badc4e1435265150d56a1660a57d653ea83dd2c
brgerig <brgerig@taylor.edu>
2020-06-04 12:58:00 -0400

    Update config.h
    Add light ID

--- config.h
+++ config.h
@@ -4,3 +4,4 @@
 // Hue settings
 #define HUE_IP                         "1.2.3.4"
 #define HUE_USER                       "HueUser"
+#define HUE_LIGHT                      3  // ID of light to control
