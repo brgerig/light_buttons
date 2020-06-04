f0147f2b936c950ab235a3c130b6c282fa1f5fc5
brgerig <brgerig@taylor.edu>
2020-06-04 11:51:34 -0400

    Update config.h
    Remove MQTT settings for Hue

--- config.h
+++ config.h
@@ -1,10 +1,2 @@
 #define SSID                          "wifissid"
 #define PASSWORD                      "wifipassword"
-
-// MQTT Settings
-#define HOSTNAME                      "hostname"
-#define MQTT_SERVER                   "mqtt.server.local"
-#define MQTT_USER                     "mqttuser"
-#define MQTT_PASS                     "mqttpass"
-#define STATE_TOPIC                   "home/name/uniquetopic"
-#define AVAILABILITY_TOPIC            "home/name/uniquetopic/available"
